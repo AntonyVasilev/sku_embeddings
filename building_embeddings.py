9c18553f8c0adbe62124b35aa836ebcb75aa8361
Anton Vasilev <antony.n.vasilev@gmail.com>
2023-08-31 12:55:46 +0300

    add als model to build items embedding

--- building_embeddings.py
+++ building_embeddings.py
@@ -1,5 +1,10 @@
 import numpy as np
+import pandas as pd
 from scipy.sparse import csr_matrix
+from implicit.cpu.als import AlternatingLeastSquares
+
+from user_item_matrix_class import UserItemMatrix
+from matrix_normalization import Normalization
 
 
 def items_embeddings(ui_matrix: csr_matrix, dim: int) -> np.ndarray:
@@ -13,4 +18,18 @@ def items_embeddings(ui_matrix: csr_matrix, dim: int) -> np.ndarray:
     Returns:
         np.ndarray: Items embeddings matrix of size (M, dim)
     """
+    als_model = AlternatingLeastSquares(factors=dim, iterations=10)
+    als_model.fit(ui_matrix)
+
+    items_vec = als_model.item_factors
     return items_vec
+
+
+if __name__ == '__main__':
+    data = pd.read_csv('data/sales_data.csv')
+    matrix_ = UserItemMatrix(data)
+    csr_matrix_ = matrix_.csr_matrix
+
+    matrix_norm = Normalization.bm_25(csr_matrix_)
+
+    items_vec_ = items_embeddings(ui_matrix=matrix_norm, dim=64)
